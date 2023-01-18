2ca96f56ee697dc28bf2f374497f2c113dfa6266
ONUR YURT <oyurt43@gmail.com>
2023-01-18 23:05:14 +0300

    first commit

---
+++ Udemy.SignalR.API/Hubs/MyHub.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Udemy.SignalR.API.Hubs
+{
+    public class MyHub:Hub
+    {
+        public static List<string> Names { get; set; } = new List<string>();
+        public async Task SetNames(string name)
+        {
+            Names.Add(name);
+            // kullanıcılara mesaj gönderme
+           await Clients.All.SendAsync("ReceiveMessage", name);
+        }
+        public async Task GetNames()
+        {
+            await Clients.All.SendAsync("ReceiveNames", Names);
+        }
+    }
+}
