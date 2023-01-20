2da2c361190ffb0f0d577b607c807d1993c8c9f1
ONUR YURT <oyurt43@gmail.com>
2023-01-20 20:17:10 +0300

    hub client

--- Udemy.SignalR.API/Hubs/MyHub.cs
+++ Udemy.SignalR.API/Hubs/MyHub.cs
@@ -1,23 +1,95 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Udemy.SignalR.API.Models;
 
 namespace Udemy.SignalR.API.Hubs
 {
     public class MyHub:Hub
     {
-        public static List<string> Names { get; set; } = new List<string>();
+        private readonly AppDbContext _context;
+        public MyHub(AppDbContext context)
+        {
+            _context = context;
+        }
+        private static List<string> Names { get; set; } = new List<string>();
+        private static int ClientCount { get; set; } = 0;
+        public static int TeamCount { get; set; } = 7;
         public async Task SendName(string name)
         {
-            Names.Add(name);
-            // kullanıcılara mesaj gönderme
-           await Clients.All.SendAsync("ReceiveName", name);
+            if (Names.Count >= TeamCount)
+            {
+                await Clients.Caller.SendAsync("Error", $"Takım en fazla {TeamCount} kişi olabilir");
+             }
+            else
+            {
+                Names.Add(name);
+                // kullanıcılara mesaj gönderme
+                await Clients.All.SendAsync("ReceiveName", name);
+            }
         }
         public async Task GetNames()
         {
             await Clients.All.SendAsync("ReceiveNames", Names);
+        }
+
+        // Grups
+
+
+        //client ı bir gruba ekleme işlemi
+        public async Task AddToGroup(string teamName)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, teamName);
+        }
+        //gruptan silme işlemi
+        public async Task RemoveGroup(string teamName)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, teamName);
+        }
+        //gruba isim gönderme
+        public async Task SendNameByGroup(string Name,string teamName)
+        {
+            var team = _context.Teams.Where(x => x.Name == teamName).FirstOrDefault();
+            if (team != null)
+            {
+                team.Users.Add(new User { Name = Name });
+            }
+            else
+            {
+                var newTeam = new Team { Name = teamName };
+                newTeam.Users.Add(new User { Name = Name });
+                _context.Teams.Add(newTeam);
+            }
+           await _context.SaveChangesAsync();
+            await Clients.Group(teamName).SendAsync("ReceiveMessageByGroup",Name,teamName);
+
+        }
+
+        //gruptaki tüm isimleri alma
+        public async Task GetNamesByGroup()
+        {
+            var teams = _context.Teams.Include(x => x.Users).Select(x => new
+            {
+                teamName = x.Name,
+                Users = x.Users.ToList()
+            });
+            await Clients.All.SendAsync("ReceiveNamesByGroup", teams);
+        }
+
+        public async override Task OnConnectedAsync()
+        {
+            ClientCount++;
+            await Clients.All.SendAsync("ReceiveClientCount", ClientCount);
+            await base.OnConnectedAsync();
+        }
+        public async override Task OnDisconnectedAsync(Exception exception)
+        {
+            ClientCount--;
+            await Clients.All.SendAsync("ReceiveClientCount", ClientCount);
+            await base.OnDisconnectedAsync(exception);
         }
     }
 }
---
+++ Udemy.SignalR.API/Models/Team.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Udemy.SignalR.API.Models
+{
+    public class Team
+    {
+        //Team.Users.add();
+        public Team()
+        {
+            Users = new List<User>();
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public virtual ICollection<User> Users { get; set; }
+    }
+}
