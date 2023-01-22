7d8278bcf895510a6fda18b7f0f2c4af7a5cafd3
ONUR YURT <57957917+OnurYURT9@users.noreply.github.com>
2023-01-22 19:05:51 +0300

    Add files via upload

---
+++ Covid19SignalR/Covid19SignalR/Controllers/CovidsController.cs
@@ -0,0 +1,41 @@
+﻿using Covid19SignalR.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Covid19SignalR.Controllers
+{
+    public class CovidsController : Controller
+    {
+        private readonly CovidService _service;
+
+        public CovidsController(CovidService service)
+        {
+            _service = service;
+        }
+        [HttpPost]
+        public async Task<IActionResult> SaveCovid(Covid covid)
+        {
+            await _service.SaveCovid(covid);
+            List<Covid> covidList = _service.GetList().ToList();
+            return Ok(covidList);
+        }
+        [HttpGet]
+        public IActionResult InitializeCovid()
+        {
+            Random rnd = new Random();
+            Enumerable.Range(1, 10).ToList().ForEach(x =>
+            {
+                foreach(ECity item in Enum.GetValues(typeof(ECity)))
+                {
+                    var newcovid = new Covid { City = item,Count= rnd.Next(100, 1000), CovidDate = DateTime.Now.AddDays(x)};
+                     _service.SaveCovid(newcovid).Wait();
+                    System.Threading.Thread.Sleep(1000);
+                }
+            });
+            return Ok("Covid-19 dataları veri tabanına kaydedildi");
+        } 
+    }
+}
---
+++ Covid19SignalR/Covid19SignalR/Models/CovidService.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Covid19SignalR.Models
+{
+    public class CovidService
+    {
+        private readonly AppDbContext _context;
+        private readonly IHubContext<CovidHub> _hubContext;
+
+        public CovidService(AppDbContext context, IHubContext<CovidHub> hubContext)
+        {
+            _context = context;
+            _hubContext = hubContext;
+        }
+        public IQueryable<Covid> GetList()
+        {
+            return _context.Covids.AsQueryable();
+        }
+        public async Task SaveCovid(Covid covid)
+        {
+            await _context.Covids.AddAsync(covid);
+            await _context.SaveChangesAsync();
+            await _hubContext.Clients.All.SendAsync("ReceiveCovidList","data");
+        }
+    }
+}
